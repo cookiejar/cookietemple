8fd09a79f0d9517fb56ca040a120a375883d5acd
Imipenem <philipp_ehm@protonmail.com>
2020-01-01 03:03:23 +0100

    Flask Fullstack template

---
+++ cookietemple/create_template/templates/website_python/{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/handlers/sites.py
@@ -0,0 +1,30 @@
+from flask import render_template, redirect, url_for, session, request
+from ..app import app
+
+
+@app.route('/')
+def root():
+    return redirect(url_for('index'))
+
+
+@app.route("/index")
+def index():
+    return render_template("index-line.html")
+
+
+@app.route('/language/<language>')
+def set_language(language=None):
+    """
+    This route is requested, whenever (and only if) the user changed the language manually
+    """
+
+    session['language'] = language
+    return redirect(url_for('index'))
+
+
+@app.before_request
+def before_request():
+    if request.url.startswith('http://'):
+        url = request.url.replace('http://', 'https://', 1)
+        code = 301
+        return redirect(url, code=code)
---
+++ cookietemple/create_template/templates/website_python/{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/server.py
@@ -0,0 +1,43 @@
+import os
+import logging
+import click
+from contextlib import suppress
+
+from gevent.pywsgi import WSGIServer
+
+from .app import app
+
+console = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console.setFormatter(formatter)
+LOG = logging.getLogger("Server")
+LOG.addHandler(console)
+LOG.setLevel(logging.INFO)
+
+CURRENT_DIR = os.path.abspath(os.getcwd())
+MODULE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_PATH = os.path.join(MODULE_DIR, 'static')
+TEMPLATES_PATH = os.path.join(MODULE_DIR, 'templates')
+
+
+@click.command()
+@click.option('-d/-p', '--debug/--production',
+              help='Enable Flask debugger', required=False)
+@click.option('-l', '--listen', default='0.0.0.0',
+              help='Listen to this address for HTTP', required=False)
+@click.option('-p', '--port', default=5000,
+              help='Listen this port for HTTP', required=False)
+def main(debug, listen, port):
+    app.template_folder = TEMPLATES_PATH
+    app.static_folder = STATIC_PATH
+
+    LOG.info("Starting service on http://%s:%d/", listen, port)
+
+    if debug:
+        LOG.warning("Running on debug mode not for production.")
+        app.run(host=listen, port=port, debug=True)
+    else:
+        http_server = WSGIServer((listen, port), app)
+
+        with suppress(KeyboardInterrupt):
+            http_server.serve_forever()
