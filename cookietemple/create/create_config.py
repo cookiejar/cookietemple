b1583a13d30ecd5d312ba1953c042ce50d26f979
Imipenem <philipp_ehm@protonmail.com>
2020-01-12 23:03:15 +0100

    Path fix common files again and again

--- cookietemple/create/create_config.py
+++ cookietemple/create/create_config.py
@@ -11,7 +11,7 @@ TEMPLATE_STRUCT = {}
 
 WD = os.path.dirname(__file__)
 TEMPLATES_PATH = f"{WD}/templates"
-COMMON_FILES_PATH1 = f"{WD}/domains/common_files"
+COMMON_FILES_PATH1 = f"{WD}/templates/common_files"
 
 
 @click.command()
