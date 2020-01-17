69e810acadcc1b6d915f74512e7c0bfea75298c8
Imipenem <philipp_ehm@protonmail.com>
2020-01-18 00:12:27 +0100

    abort

--- cookietemple/create/create_config.py
+++ cookietemple/create/create_config.py
@@ -12,7 +12,7 @@ from cookiecutter.main import cookiecutter
 # The main dictionary, which will be completed by first the general options prompts and then the chosen template
 # specific prompts. It is then passed onto cookiecutter as extra_content to facilitate the template creation.
 # Finally, it is also used for the creation of the .cookietemple file.
-from cookietemple import cookietemple_cli
+from cookietemple.cookietemple_cli import cookietemple_cli
 
 TEMPLATE_STRUCT = {}
 
