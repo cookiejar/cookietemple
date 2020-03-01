adcb7ef055fdac032dd47f64158921b9171e2fc0
Zethson <lukas.heumos@posteo.net>
2020-03-01 13:57:33 +0100

    [FIX] universal newlines & no more username/password prompts

--- cookietemple/create/github_support.py
+++ cookietemple/create/github_support.py
@@ -1,4 +1,3 @@
-import os
 from os import path
 from subprocess import Popen, PIPE
 
@@ -24,7 +23,8 @@ def run(github_username, github_password, private):
         repo = user.create_repo('testrepo_github_api', description='this is a test', private=private)
 
     repository = path.dirname('/tmp/test_clone/')
-    git_query = Popen(['/usr/bin/git', 'clone', 'https://github.com/Zethson/testrepo_github_api', repository], stdout=PIPE, stderr=PIPE)
+    git_query = Popen(['/usr/bin/git', 'clone', f'https://{github_username}:{github_password}@github.com/{github_username}/testrepo_github_api', repository],
+                      stdout=PIPE, stderr=PIPE, universal_newlines=True)
     (git_status, error) = git_query.communicate()
 
     # create some dummy data
@@ -32,13 +32,14 @@ def run(github_username, github_password, private):
         f.write('wtf')
 
     print(f"CLONE: {git_status} ___ {error}")
-    git_query = Popen(['git', 'add', '.'], cwd=repository, stdout=PIPE, stderr=PIPE)
+    git_query = Popen(['git', 'add', '.'], cwd=repository, stdout=PIPE, stderr=PIPE, universal_newlines=True)
     (git_status, error) = git_query.communicate()
     print(f"ADD: {git_status} ___ {error}")
-    git_query = Popen(['git', 'commit', '-m', r'"bla"'], cwd=repository, stdout=PIPE, stderr=PIPE)
+    git_query = Popen(['git', 'commit', '-m', r'"bla"'], cwd=repository, stdout=PIPE, stderr=PIPE, universal_newlines=True)
     (git_status, error) = git_query.communicate()
     print(f"COMMIT: {git_status} ___ {error}")
-    git_query = Popen(['git', 'push', '-u', 'origin', 'master'], cwd=repository, stdout=PIPE, stderr=PIPE, universal_newlines=True)
+    git_query = Popen(['git', 'push', '-u', f'https://{github_username}:{github_password}@github.com/{github_username}/testrepo_github_api', '--all'],
+                      cwd=repository, stdout=PIPE, stderr=PIPE, universal_newlines=True)
     (git_status, error) = git_query.communicate()
     print(f"PUSH: {git_status} ___ {error}")
 
---
+++ cookietemple/create/test_history.py
@@ -0,0 +1,5 @@
+from os import path
+from subprocess import Popen, PIPE
+
+import os
+print(os.environ['SHELL'])
