807c51cebe1ed11c7d8130041b63aa9d683995b1
Zethson <lukas.heumos@posteo.net>
2020-03-14 11:53:42 +0100

    [FIX] formatting of comments

--- tests/create/test_create_general_configs.py
+++ tests/create/test_create_general_configs.py
@@ -25,12 +25,10 @@ def init_template_struct() -> set:
     return TEMPLATE_STRUCT
 
 
-"""
-    Ensure that valid inputs for the genereal prompts for all template are processed properly.
-"""
-
-
 def test_general_prompts_all_input_valid(monkeypatch) -> None:
+    """
+    Ensure that valid inputs for the genereal prompts for all template are processed properly.
+    """
     prompts = StringIO('MyFullName\nMyEmail\nMyProjectName\nMyDesc\nMyVersion\nMIT')
     monkeypatch.setattr('sys.stdin', prompts)
     prompt_general_template_configuration()
@@ -40,12 +38,10 @@ def test_general_prompts_all_input_valid(monkeypatch) -> None:
             and TEMPLATE_STRUCT['version'] == 'MyVersion' and TEMPLATE_STRUCT['license'] == 'MIT')
 
 
-"""
-    Ensure that entering an invalid license will trigger an error message.
-"""
-
-
 def test_general_prompts_with_license_invalid_choice(monkeypatch, capfd) -> None:
+    """
+    Ensure that entering an invalid license will trigger an error message.
+    """
     prompts = StringIO('MyFullName\nMyEmail\nMyProjectName\nMyDesc\nMyVersion\nIMALICENSE\nMIT')
     monkeypatch.setattr('sys.stdin', prompts)
     prompt_general_template_configuration()
@@ -53,12 +49,10 @@ def test_general_prompts_with_license_invalid_choice(monkeypatch, capfd) -> None
     assert 'Error: invalid choice: IMALICENSE.' in out.strip()
 
 
-"""
-    Ensure that the .cookietemple file is created using the right arguments.
-"""
-
-
 def test_create_dot_cookietemple_file() -> None:
+    """
+    Ensure that the .cookietemple file is created using the right arguments.
+    """
     open_mock = mock_open()
     with patch("cookietemple.create.create_templates.open", open_mock, create=True):
         create_dot_cookietemple(TEMPLATE_STRUCT, "MyOtherVersion", "MyOtherHandle")
@@ -66,12 +60,10 @@ def test_create_dot_cookietemple_file() -> None:
     open_mock.assert_called_with(f'{TEMPLATE_STRUCT["project_name"]}/.cookietemple', "w")
 
 
-"""
-    Ensure that this function deletes a random directory with subdirectories and files
-"""
-
-
 def test_del_dir_tree(tmp_path) -> None:
+    """
+    Ensure that this function deletes a random directory with subdirectories and files
+    """
     dir = tmp_path / "testIT"
     a = dir / "a"
     b = dir / "b"
@@ -87,14 +79,13 @@ def test_del_dir_tree(tmp_path) -> None:
     assert len(list(tmp_path.iterdir())) == 0
 
 
-# mock current working directory for common_files tests TODO:FIX TEST
-"""
-    This test should test a isolated common files creation function.
-"""
-
-
 @pytest.mark.skip(reason="Fix this test later on")
 def test_create_common_files(mocker, tmp_path) -> None:
+    """
+    This test should test a isolated common files creation function.
+    """
+    # mock current working directory for common_files tests TODO:FIX TEST
+
     mocker.patch.object(Path, 'cwd', autospec=True)
     mocker.patch.object(os, 'getcwd', autospec=True)
     mocker.patch.object(tempfile, 'mkdtemp', autospec=True)
