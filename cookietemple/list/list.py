31b9c1eba751b1dfa3b5120b316c8904b16903b6
Zethson <lukas.heumos@posteo.net>
2020-03-21 15:20:32 +0100

    [REFACTORING]

--- cookietemple/list/list.py
+++ cookietemple/list/list.py
@@ -6,13 +6,13 @@ from pathlib import Path
 from ruamel.yaml import YAML
 from tabulate import tabulate
 
-from cookietemple.util.dict_util import delete_keys_from_dict, is_nested_dictionary
+from cookietemple.util.dict_util import is_nested_dictionary
 
 WD = os.path.dirname(__file__)
 TEMPLATES_PATH = f'{WD}/../create/templates'
 
 
-def list_available_templates():
+def list_available_templates() -> None:
     """
     Displays all available templates to stdout in nicely formatted yaml format.
     Omits long descriptions.
@@ -20,8 +20,6 @@ def list_available_templates():
     """
 
     available_templates = load_available_templates(f'{TEMPLATES_PATH}/available_templates.yaml')
-    # listing does not need to display the long descriptions of the templates
-    # users should use info for long descriptions
     click.echo(click.style('Run cookietemple info for long descriptions of your template of interest.', fg='green'))
     click.echo(click.style('All available templates:\n', fg='green'))
 
@@ -42,10 +40,11 @@ def list_available_templates():
                     val['name'], val['handle'], val['short description'], val['available libraries'], val['version']
                 ])
 
+    # Print nicely to console
     click.echo(tabulate(templates_to_tabulate, headers=['Name', 'Handle', 'Short Description', 'Available Libraries', 'Version']))
 
 
-def load_available_templates(AVAILABLE_TEMPLATES_PATH):
+def load_available_templates(AVAILABLE_TEMPLATES_PATH) -> dict:
     """
     Loads 'available_templates.yaml' as a yaml file and returns the content as nested dictionary.
 
