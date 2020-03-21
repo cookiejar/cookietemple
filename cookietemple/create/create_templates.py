6ec5af17a801ca48b3c9bdbbb671d36ccdae2918
Imipenem <philipp_ehm@protonmail.com>
2020-03-21 15:42:39 +0100

    Set init version

--- cookietemple/create/create_templates.py
+++ cookietemple/create/create_templates.py
@@ -138,19 +138,3 @@ def directory_exists_warning() -> None:
     click.echo()
     click.echo(click.style('Proceeding now will overwrite this directory and its content!', fg='red'))
     click.echo()
-
-
-def update_version_on_create(cfg_file_path: Path, initial_version: str) -> None:
-    """
-    This function updates the version in the bump_version.cfg file when a new template has been created with
-    the version the user entered as initial project version.
-    :param cfg_file_path: The path where the bump_version.cfg file is located.
-    :param initial_version: The initial version number entered by the project creator.
-    """
-
-    parser = SafeConfigParser()
-    parser.read(f'{cfg_file_path}')
-
-    parser.set('bumpversion', 'current_version', initial_version)
-    with open(f'{cfg_file_path}', 'w') as configfile:
-        parser.write(configfile)
