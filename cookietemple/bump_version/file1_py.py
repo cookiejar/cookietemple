d9935c881a1865c628271ae59c8c3eb1cd8787a8
Imipenem <philipp_ehm@protonmail.com>
2020-03-21 14:30:30 +0100

    Removed test files from bump_version dir

--- cookietemple/bump_version/file1_py.py
+++
@@ -1,7 +0,0 @@
-AVAILABLE_VERSION = "v0.0.0"  # <<COOKIETEMPLE_NO_BUMP>>
-
-
-def hello_world():
-    version = "v0.0.0"
-    version2 = "0.0.0dev"
-    return version + version2
--- cookietemple/bump_version/file2_java.java
+++
@@ -1,9 +0,0 @@
-public static class idk {
-
-    static no_clue_how_to_write_java_anymore = v0.0.0
-
-    static no_clue_how_to_write_java_anymore = 0.0.0
-
-    static no_clue_how_to_write_java_anymore = v0.0.0 // <<COOKIETEMPLE_NO_BUMP>>
-
-}
