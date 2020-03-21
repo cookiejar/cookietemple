a25573535d462b9ba865f4f22c56aa8dfd36eed6
Zethson <lukas.heumos@posteo.net>
2020-03-21 20:50:24 +0100

    [FEATURE] file exist linting!

--- cookietemple/linting/PipelineLint.py
+++ cookietemple/linting/PipelineLint.py
@@ -35,7 +35,7 @@ class PipelineLint(object):
         Raises:
             If a critical problem is found, an ``AssertionError`` is raised.
         """
-        with click.progressbar(check_functions, label=label, item_show_func=repr) as function_names:  #  item_show_func=repr leads to some Nones in the bar
+        with click.progressbar(check_functions, label=label, item_show_func=repr) as function_names:  # item_show_func=repr leads to some Nones in the bar
             for fun_name in function_names:
                 getattr(calling_class, fun_name)()
                 if len(self.failed) > 0:
@@ -47,94 +47,96 @@ class PipelineLint(object):
         Iterates through the pipeline's directory content and checkmarks files
         for presence.
         Files that **must** be present::
-            'nextflow.config',
             'Dockerfile',
-            ['LICENSE', 'LICENSE.md', 'LICENCE', 'LICENCE.md'], # NB: British / American spelling
-            'README.md',
-            'CHANGELOG.md',
-            'docs/README.md',
-            'docs/output.md',
-            'docs/usage.md'
+            'cookietemple.cfg'
+            'Makefile'
+            'README.rst'
+            'CHANGELOG.rst'
+            '[LICENSE, LICENSE.md, LICENCE, LICENCE.md]'
+            'docs/index.rst'
+            'docs/readme.rst'
+            'docs/changelog.rst'
+            'docs/installation.rst'
+            'docs/usage.rst'
         Files that *should* be present::
-            'main.nf',
-            'environment.yml',
-            'conf/base.config',
-            '.github/workflows/branch.yml',
-            '.github/workflows/ci.yml',
-            '.github/workfows/linting.yml'
+            '.coafile',
+            '.gitignore',
+            '.dependabot/config.yml',
+            '.github/ISSUE_TEMPLATE/bug_report.md',
+            '.github/ISSUE_TEMPLATE/feature_request.md',
         Files that *must not* be present::
-            'Singularity'
+            none
         Files that *should not* be present::
             '.travis.yml'
         Raises:
-            An AssertionError if neither `nextflow.config` or `main.nf` found.
+            An AssertionError if .cookietemple is not found found.
         """
 
         # NB: Should all be files, not directories
         # List of lists. Passes if any of the files in the sublist are found.
         files_fail = [
-            ['nextflow.config'],
             ['Dockerfile'],
+            ['cookietemple.cfg'],
+            ['Makefile'],
+            ['README.rst'],
+            ['CHANGELOG.rst'],
             ['LICENSE', 'LICENSE.md', 'LICENCE', 'LICENCE.md'],  # NB: British / American spelling
-            ['README.md'],
-            ['CHANGELOG.md'],
-            [os.path.join('docs', 'README.md')],
-            [os.path.join('docs', 'output.md')],
-            [os.path.join('docs', 'usage.md')],
-            [os.path.join('.github', 'workflows', 'branch.yml')],
-            [os.path.join('.github', 'workflows', 'ci.yml')],
-            [os.path.join('.github', 'workflows', 'linting.yml')]
+            [os.path.join('docs', 'index.rst')],
+            [os.path.join('docs', 'readme.rst')],
+            [os.path.join('docs', 'changelog.rst')],
+            [os.path.join('docs', 'installation.rst')],
+            [os.path.join('docs', 'usage.rst')],
         ]
         files_warn = [
-            ['main.nf'],
-            ['environment.yml'],
-            [os.path.join('conf', 'base.config')]
+            ['.coafile'],
+            ['.gitignore'],
+            ['.dependabot/config.yml'],
+            [os.path.join('.github', 'ISSUE_TEMPLATE', 'bug_report.md')],
+            [os.path.join('.github', 'ISSUE_TEMPLATE', 'feature_request.md')]
         ]
 
         # List of strings. Dails / warns if any of the strings exist.
         files_fail_ifexists = [
-            'Singularity'
+
         ]
         files_warn_ifexists = [
             '.travis.yml'
         ]
 
-
-        self.warned.append((1, "File not found: blub"))
-        # First - critical files. Check that this is actually a Nextflow pipeline
-
+        # First - critical files. Check that this is actually a COOKIETEMPLE based project
         if not os.path.isfile(pf(self, '.cookietemple')):
             raise AssertionError('.cookietemple not found!! Is this a COOKIETEMPLE project?')
 
-    # # Files that cause an error if they don't exist
-    # for files in files_fail:
-    #     if any([os.path.isfile(pf(f)) for f in files]):
-    #         self.passed.append((1, "File found: {}".format(self._bold_list_items(files))))
-    #         self.files.extend(files)
-    #     else:
-    #         self.failed.append((1, "File not found: {}".format(self._bold_list_items(files))))
-    #
-    # # Files that cause a warning if they don't exist
-    # for files in files_warn:
-    #     if any([os.path.isfile(pf(f)) for f in files]):
-    #         self.passed.append((1, "File found: {}".format(self._bold_list_items(files))))
-    #         self.files.extend(files)
-    #     else:
-    #         self.warned.append((1, "File not found: {}".format(self._bold_list_items(files))))
-    #
-    # # Files that cause an error if they exist
-    # for file in files_fail_ifexists:
-    #     if os.path.isfile(pf(file)):
-    #         self.failed.append((1, "File must be removed: {}".format(self._bold_list_items(file))))
-    #     else:
-    #         self.passed.append((1, "File not found check: {}".format(self._bold_list_items(file))))
-    #
-    # # Files that cause a warning if they exist
-    # for file in files_warn_ifexists:
-    #     if os.path.isfile(pf(file)):
-    #         self.warned.append((1, "File should be removed: {}".format(self._bold_list_items(file))))
-    #     else:
-    #         self.passed.append((1, "File not found check: {}".format(self._bold_list_items(file))))
+        # Files that cause an error if they don't exist
+        for files in files_fail:
+            if any([os.path.isfile(pf(self, f)) for f in files]):
+                self.passed.append((1, "File found: {}".format(self._bold_list_items(files))))
+                self.files.extend(files)
+            else:
+                self.failed.append((1, "File not found: {}".format(self._bold_list_items(files))))
+
+        # Files that cause a warning if they don't exist
+        for files in files_warn:
+            if any([os.path.isfile(pf(self, f)) for f in files]):
+                self.passed.append((1, "File found: {}".format(self._bold_list_items(files))))
+                self.files.extend(files)
+            else:
+                self.warned.append((1, "File not found: {}".format(self._bold_list_items(files))))
+
+        # Files that cause an error if they exist
+        for file in files_fail_ifexists:
+            if os.path.isfile(pf(self, file)):
+                self.failed.append((1, "File must be removed: {}".format(self._bold_list_items(file))))
+            else:
+                self.passed.append((1, "File not found check: {}".format(self._bold_list_items(file))))
+
+        # Files that cause a warning if they exist
+        for file in files_warn_ifexists:
+            if os.path.isfile(pf(self, file)):
+                self.warned.append((1, "File should be removed: {}".format(self._bold_list_items(file))))
+            else:
+                self.passed.append((1, "File not found check: {}".format(self._bold_list_items(file))))
+
 
     def print_results(self):
         click.echo("{}\n          LINTING RESULTS\n{}\n".format(click.style('=' * 35, dim=True), click.style('=' * 35, dim=True))
@@ -159,7 +161,7 @@ class PipelineLint(object):
         if len(self.warned) > 0:
             click.echo(click.style(f'Test Warnings: \n {format_result(self.warned)}', fg='yellow'))
         if len(self.failed) > 0:
-            click.echo(click.style(f'Test Failures: \n {format_result(self.passed)}', fg='red'))
+            click.echo(click.style(f'Test Failures: \n {format_result(self.failed)}', fg='red'))
 
     def _bold_list_items(self, files):
         if not isinstance(files, list):
