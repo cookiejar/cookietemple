07fee03f927c43c4ab5bac0f6b27afa5650d6a43
Imipenem <philipp_ehm@protonmail.com>
2020-03-23 20:13:53 +0100

    Try to fix import error

--- cookietemple/cookietemple_cli.py
+++ cookietemple/cookietemple_cli.py
@@ -8,8 +8,7 @@ import click
 import re
 from pathlib import Path
 
-import cookietemple
-
+from . import __version__
 from cookietemple.bump_version.bump_version import bump_template_version
 from cookietemple.create.create import choose_domain
 from cookietemple.info.info import show_info
@@ -38,7 +37,7 @@ def main():
 
 
 @click.group(cls=CustomHelpOrder)
-@click.version_option(cookietemple.__version__)
+@click.version_option(__version__)
 @click.option(
     '-v', '--verbose',
     is_flag=True,
