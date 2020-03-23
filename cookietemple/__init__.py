357cf9c5dfb68c011dbf4e5cf031ae9862b540a8
Imipenem <philipp_ehm@protonmail.com>
2020-03-23 20:29:35 +0100

    3.8 fix???again

--- cookietemple/__init__.py
+++ cookietemple/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 try:
-    import importlib.metadata
+    import importlib.metadata as metadata
 except ImportError:
     # Running on pre-3.8 Python; use importlib-metadata package
     import importlib_metadata as metadata
