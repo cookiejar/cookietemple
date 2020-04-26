49d16cce5b59c7ab9580cb924731412dad158a5a
Imipenem <philipp_ehm@protonmail.com>
2020-04-26 21:34:24 +0200

    Minor refactoring

--- cookietemple/util/click_util.py
+++ cookietemple/util/click_util.py
@@ -19,7 +19,7 @@ class CustomHelpOrder(click.Group):
         return super(CustomHelpOrder, self).get_help(ctx)
 
     def list_commands_for_help(self, ctx):
-        '""reorder the list of commands when listing the help""'
+        """reorder the list of commands when listing the help"""
         commands = super(CustomHelpOrder, self).list_commands(ctx)
         return (c[1] for c in sorted((self.help_priorities.get(command, 1000), command) for command in commands))
 
@@ -37,15 +37,24 @@ class CustomHelpOrder(click.Group):
         return decorator
 
     def get_command(self, ctx, cmd_name):
+        """
+        Override the get_command of Click.
+        If an unknown command is given, try to determine a similar command.
+        If no similar command could´ve been found, exit with an error message.
+        Else use the most similar command while printing a status message for the user.
+
+        :param ctx: The given Click context for the group
+        :param cmd_name: The command invoked with Click
+        """
         rv = click.Group.get_command(self, ctx, cmd_name)
         if rv is not None:
             return rv
         sim_commands = most_similar_command(cmd_name, MAIN_COMMANDS)
 
-        matches = [x for x in self.list_commands(ctx) if x in sim_commands]
+        matches = [cmd for cmd in self.list_commands(ctx) if cmd in sim_commands]
         if not matches:
             ctx.fail(click.style('Unknown command and no similar command was found!', fg='red'))
         elif len(matches) == 1:
             click.echo(click.style(f'Unknown command! Will use best match {matches[0]}.', fg='red'))
             return click.Group.get_command(self, ctx, matches[0])
-        ctx.fail(click.style('Unknown command. Most similar commands were %s' % ', '.join(sorted(matches)), fg='red'))
+        ctx.fail(click.style(f'Unknown command. Most similar commands were {", ".join(sorted(matches))}', fg='red'))
