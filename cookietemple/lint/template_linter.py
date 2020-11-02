19ef224da1d3ce16c0bdd7a74e54b3b2713f81cd
Lukas Heumos <lukas.heumos@posteo.net>
2020-11-02 15:19:41 +0100

    Update template_linter.py

--- cookietemple/lint/template_linter.py
+++ cookietemple/lint/template_linter.py
@@ -222,7 +222,7 @@ class TemplateLinter(object):
                 with io.open(os.path.join(root, fname), 'rt', encoding='latin1') as file:
                     for line in file:
                         # TODO We should also add some of the more advanced cookiecutter if statements, raw statements etc
-                        regex = re.compile('{\s*[%](?s).* cookiecutter(?s).*[%]\s*}')
+                        regex = re.compile(r'{\s?[%]\s?.* cookiecutter\s?.*[%]\s?}')  # noqa W605
                         if regex.match(line):
                             line = f'{line[:50 - len(fname)]}..'
                             self.warned.append(('general-4', f'Cookiecutter string found in \'{fname}\': {line}'))
