7eeb031baa2a77b5b5b9cd1e1a8d85e232ca6c1a
Lukas Heumos <lukas.heumos@posteo.net>
2020-11-03 10:28:05 +0100

    fixed license in setup.py

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ setup(
         ],
     },
     install_requires=requirements,
-    license='Apache 2.0',
+    license='Apache License 2.0',
     long_description=readme,
     include_package_data=True,
     keywords='cookietemple',
