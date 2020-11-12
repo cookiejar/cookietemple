668426749b7d6e917451fb5ed4ab695a24fa2fe0
Zethson <lukas.heumos@posteo.net>
2020-11-12 19:39:26 +0100

    reeview
    Signed-off-by: Zethson <lukas.heumos@posteo.net>

--- cookietemple/common/suggest_similar_commands.py
+++ cookietemple/common/suggest_similar_commands.py
@@ -24,7 +24,7 @@ def load_available_handles() -> set:
     """
     available_templates = load_yaml_file(f'{AVAILABLE_TEMPLATES_PATH}')
     unsplit_handles: Set[str] = set()
-    all_handles = set()
+    all_handles: Set[str] = set()
     nested_dict_to_handle_set(available_templates, unsplit_handles)
     all_handles.update(unsplit_handles)
     split_handles(unsplit_handles, all_handles)
