da96018704ae6abb0e1504a1ce4ede3e62e4bb3a
Imipenem <philipp_ehm@protonmail.com>
2020-11-20 19:35:07 +0100

    Refactored deployment process for web template + updated docs

--- cookietemple/create/templates/web/website_python/flask/{{ cookiecutter.project_slug_no_hyphen }}/deployment_scripts/setup.sh
+++ cookietemple/create/templates/web/website_python/flask/{{ cookiecutter.project_slug_no_hyphen }}/deployment_scripts/setup.sh
@@ -1,29 +1,42 @@
 #!/bin/bash
 # Reference:
 # https://www.digitalocean.com/community/tutorials/how-to-serve-flask-applications-with-gunicorn-and-nginx-on-ubuntu-18-04
+sudo apt-get install software-properties-common
+sudo apt-add-repository universe
+sudo apt-get update
+sudo apt-get install python3-pip
+sudo apt-get install python3-dev nginx -y
 
-cp /home/{{cookiecutter.vmusername}}/{{cookiecutter.project_slug}}/deployment_scripts/{{cookiecutter.project_slug_no_hyphen}}.service \
+sudo pip3 install virtualenv
+virtualenv dpenv
+source dpenv/bin/activate
+
+sudo apt-get update
+pip3 install gunicorn
+make install
+
+sudo cp /home/{{cookiecutter.vmusername}}/{{cookiecutter.project_slug}}/deployment_scripts/{{cookiecutter.project_slug_no_hyphen}}.service \
 /etc/systemd/system/{{cookiecutter.project_slug_no_hyphen}}.service
 
-systemctl start {{cookiecutter.project_slug}}
+sudo systemctl start {{cookiecutter.project_slug}}
 
-systemctl enable {{cookiecutter.project_slug}}
+sudo systemctl enable {{cookiecutter.project_slug}}
 
-cp /home/{{cookiecutter.vmusername}}/{{cookiecutter.project_slug}}/deployment_scripts/{{cookiecutter.project_slug_no_hyphen}} \
+sudo cp /home/{{cookiecutter.vmusername}}/{{cookiecutter.project_slug}}/deployment_scripts/{{cookiecutter.project_slug_no_hyphen}} \
 /etc/nginx/sites-available/{{cookiecutter.project_slug_no_hyphen}}
 
-ln -s /etc/nginx/sites-available/{{cookiecutter.project_slug_no_hyphen}} /etc/nginx/sites-enabled
+sudo ln -s /etc/nginx/sites-available/{{cookiecutter.project_slug_no_hyphen}} /etc/nginx/sites-enabled
 
-nginx -t
+sudo nginx -t
 
-systemctl restart nginx
+sudo systemctl restart nginx
 
-ufw delete allow 5000
+sudo ufw delete allow 5000
 
-ufw allow 'Nginx Full'
+sudo ufw allow 'Nginx Full'
 
-add-apt-repository ppa:certbot/certbot -y
+sudo add-apt-repository ppa:certbot/certbot -y
 
-apt install python3-certbot-nginx -y
+sudo apt install python3-certbot-nginx -y
 
-certbot --nginx -d {{cookiecutter.url}} -d www.{{cookiecutter.url}} --non-interactive --agree-tos -m {{cookiecutter.email}}
+sudo certbot --nginx -d {{cookiecutter.url}} -d www.{{cookiecutter.url}} --non-interactive --agree-tos -m {{cookiecutter.email}}
