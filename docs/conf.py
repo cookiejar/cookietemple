bc3e7d0bd041858a6e68162bc9ab585725450f60
Imipenem <philipp_ehm@protonmail.com>
2021-05-07 20:58:17 +0200

    Fixed pre-commits and version inconsistency

--- docs/conf.py
+++ docs/conf.py
@@ -22,8 +22,8 @@ project = u"cookietemple"
 copyright = u"2020, Lukas Heumos, Philipp Ehmele, the cookiejar organization"
 author = u"Lukas Heumos, Philipp Ehmele, the cookiejar organization"
 
-version = '1.3.4'
-release = '1.3.4'
+version = "1.3.4"
+release = "1.3.4"
 
 language = None
 
