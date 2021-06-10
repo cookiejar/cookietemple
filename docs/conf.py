eeff88694d263a103158a62e4a157d6b5595b182
zethson <lukas.heumos@posteo.net>
2021-06-10 13:53:15 +0200

    Bump version from 1.3.5 to 1.3.6

--- docs/conf.py
+++ docs/conf.py
@@ -23,8 +23,8 @@ project = u"cookietemple"
 copyright = u"2020, Lukas Heumos, Philipp Ehmele, the cookiejar organization"
 author = u"Lukas Heumos, Philipp Ehmele, the cookiejar organization"
 
-version = "1.3.5"
-release = "1.3.5"
+version = "1.3.6"
+release = "1.3.6"
 
 language = None
 
