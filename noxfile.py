b00dfc43a0f3b70fc21da1b318a52f94e9e51ad6
zethson <lukas.heumos@posteo.net>
2022-03-27 20:15:36 +0200

    fix CT nox
    Signed-off-by: zethson <lukas.heumos@posteo.net>

--- noxfile.py
+++ noxfile.py
@@ -170,7 +170,7 @@ def docs(session: Session) -> None:
     """Build and serve the documentation with live reloading on file changes."""
     args = session.posargs or ["--open-browser", "docs", "docs/_build"]
     session.install(".")
-    session.install("sphinx", "sphinx-autobuild", "sphinx-click", "sphinx-rtd-theme", "sphinx-rtd-dark-mode")
+    session.install("sphinx", "sphinx-autobuild", "sphinx-click", "furo")
 
     build_dir = Path("docs", "_build")
     if build_dir.exists():
