ca8ed0638799d0901faad5867fe6054a457ecb3c
zethson <lukas.heumos@posteo.net>
2023-01-23 00:41:42 +0100

    Poetry lock & darglint removal
    Signed-off-by: zethson <lukas.heumos@posteo.net>

--- noxfile.py
+++ noxfile.py
@@ -81,7 +81,6 @@ def precommit(session: Session) -> None:
     args = session.posargs or ["run", "--all-files"]
     session.install(
         "black",
-        "darglint",
         "flake8",
         "flake8-bandit",
         "flake8-bugbear",
